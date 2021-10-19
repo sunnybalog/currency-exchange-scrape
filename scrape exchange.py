7689b0b0cd889cbbe17236173308f042f9d89a2a
sunnybalog <33362184+sunnybalog@users.noreply.github.com>
2021-10-19 15:59:18 +0100

    Update scrape exchange.py
    --version 1.2

--- scrape exchange.py
+++ scrape exchange.py
@@ -4,47 +4,47 @@ import sqlite3
 from datetime import date, datetime, timedelta
 from threading import Timer
 
-
+# capture current date and time of startup of the python script
 x = datetime.now()
+# automatically scrape the webpage for define data at exactly 9AM Local time everyday
 y = x.replace(day=x.day, hour=9, minute=0, second=0, microsecond=0) + timedelta(days=1)
 delta_t = y - x
 secs = delta_t.total_seconds()
 
 
 def hello_world():
+    currency = 'NGN'
     current_date = date.today()
-    conn = sqlite3.connect('cputest.db')
+    conn = sqlite3.connect('learn.db')
     c = conn.cursor()
-    # c.execute('''CREATE TABLE exchanger(date DATE, ngn REAL)''')
+
+    # next two lines used to create new table, comment them out after creating the table
+    # c.execute('''CREATE TABLE real_exchange (current_date DATE, currency TEXT, the_exchange REAL)''')
     # conn.commit()
+
     url = "https://www.tpsgc-pwgsc.gc.ca/cgi-bin/recgen/er.pl?Language=E"
     r = requests.get(url)
     soup = bS(r.content, 'html.parser')
+
+    paragraphs = soup.find_all('td')
     n = 0
-    for tag in soup.find_all('td'):
-        text_extract = ''.join(tag.findAll(text=True))
-        if n == 173:
-            NGN = float(text_extract)
+    for p in paragraphs:
+        if p.text == currency:
+            the_index = n
+            the_exchange = float(paragraphs[the_index + 2].text)
+            c.execute('''INSERT INTO real_exchange VALUES(?, ?, ?)''', (current_date, currency, the_exchange))
+            conn.commit()
+
+            # to display table data in python console, if not comment next two lines out
+            for row in c.execute('SELECT * FROM real_exchange ORDER BY current_date'):
+                print(row)
         else:
-            pass
-        n = n + 1
-    c.execute('''INSERT INTO exchanger VALUES(?, ?)''', (current_date, NGN))
-    conn.commit()
-    for row in c.execute('SELECT * FROM exchanger ORDER BY date'):
-        # results = c.fetchall()
-        # print(results)
-        print(row)
-    # c.execute('''DELETE FROM exchanger''')
-    # conn.commit()
+            n = n + 1
 
 
 t = Timer(secs, hello_world)
 t.start()
 
 # query to delete all data with specific rowID
-# c.execute('''DELETE from exchanger where DATE="2021-10-17"''')
-# conn.commit()
-
-# create new database table, comment out after creation to avoid error
-# c.execute('''CREATE TABLE exchanger(date DATE, ngn REAL)''')
+# c.execute('''DELETE from real_exchange where DATE="2021-10-17"''')
 # conn.commit()
