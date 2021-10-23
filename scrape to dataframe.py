e084ea5bcb77a80d361b30afd8d19c595ac9937a
sunnybalog <33362184+sunnybalog@users.noreply.github.com>
2021-10-23 12:52:26 +0100

    Update scrape to dataframe.py

--- scrape to dataframe.py
+++ scrape to dataframe.py
@@ -4,20 +4,25 @@ from datetime import date, datetime, timedelta
 from threading import Timer
 import pandas as pd
 
+# create timestamp to capture date&time of first run and subsequent run of the script
 x = datetime.today()
 y = x.replace(day=x.day, hour=12, minute=0, second=0, microsecond=0) + timedelta(days=1)
 delta_t = y - x
 secs = delta_t.total_seconds()
 
-
+#unction to run the loop
 def scrape_cdn_exchange():
+    # url to scrape
     URL = "https://www.tpsgc-pwgsc.gc.ca/cgi-bin/recgen/er.pl?Language=E"
+    # send requent to the webpage
     web_content = requests.get(URL)
 
     soup = BeautifulSoup(web_content.content, 'html5lib')
+    # create format for columns
     df = pd.DataFrame(columns=['Currency', 'Description', 'Rate'])
 
     table = soup.find('table', {'class': 'table table-hover table-bordered'})
+    # Loop through the table in the webpage and scrape data row by row
     for row in table.find_all('tr'):
         columns = row.find_all('td')
         if columns:
@@ -26,13 +31,13 @@ def scrape_cdn_exchange():
             rate = float(columns[2].text.strip())
             df = df.append({'Currency': currency, 'Description': description, 'Rate': rate}, ignore_index=True)
 
-# create csv with filename as the date and timestamp that the data got scraped
+# create csv file with filename as the date that the data got scraped
     filename = str(date.today()) + '_.csv'
 
 # write data into the file
     df.to_csv(filename, encoding='utf-8')
 
-# print to python if interested to see few lines of scrape data
+# print to python console if interested to see few lines of scrape data
     print(df.head())
 
 
